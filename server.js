5176bdf3b511a1a15d167bc6d1ee20c5ae6b81f8
FlavioHPOliveira <flaviohenri7@hotmail.com>
2022-01-05 09:17:13 -0300

    set up websocket

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./app')
+const PORT=process.env.PORT || 3000;
+const socket = require('socket.io');
+
+
+const server = app.listen(PORT, ()=>{
+  console.log('hello');
+});
+
+const io=socket(server);
+
+io.on('connect', function(socket){
+
+  console.log('un cliente se conecto'+ ' ' +socket.id);
+  
+
+})
